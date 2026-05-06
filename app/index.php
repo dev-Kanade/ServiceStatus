a400cc1cfc2878157446ac76cfab1b9a093332d1
dev-kanade <kanade.miyachi@gmail.com>
2026-05-06 19:20:07 +0900

    軽く

--- app/index.php
+++ app/index.php
@@ -1,3 +1,16 @@
 <?php
-
+$color = "skyblue";
+$size = 150;
 ?>
+
+<div style="
+    width: <?php echo $size; ?>px; 
+    height: <?php echo $size; ?>px; 
+    background-color: <?php echo $color; ?>; 
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+">
+    PHPで描いた円
+</div>
